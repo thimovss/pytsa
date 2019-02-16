9f45d34e0afe08dea805d53d9311a5d11eb59cf4
Thimovss <thimo.visser@hotmail.nl>
2019-02-16 11:23:38 +0100

    added setup.cfg for PBR

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='pytsa',
-    author="Thimo Visser",
-    author_email="thimo.visser@gmail.com",
-    description="simple, human readable decorators package to ensure your method abides to it's contract",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/thimovss/pytsa",
-    license="MIT",
-    packages=setuptools.find_packages(),
-    keywords="type strict arguments rules",
-    classifiers=[
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Utilities",
-    ],
-    project_urls={
-        "Source Code": "https://github.com/thimovss/pytsa",
-    }
+setup(
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
